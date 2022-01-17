33811788273de02431ba258524de6b013c32b858
Xiangxian Zhou <xiangxianzhou@gmail.com>
2022-01-17 15:55:52 +0800

    testaddress

--- src/component/Personinfo.js
+++ src/component/Personinfo.js
@@ -3,6 +3,7 @@ import logo from "../resource/logo.png"
 import name_cn from "../resource/name_cn.png"
 import { AiFillGithub, AiOutlineMail, AiFillLinkedin } from 'react-icons/ai';
 import { SiGooglescholar } from 'react-icons/si'
+import { FiMapPin } from 'react-icons/fi'
 
 const Personinfo = () => {
     return (
@@ -16,8 +17,8 @@ const Personinfo = () => {
                 D. Eng in EE<br />
                 Power System Researcher<br />
                 MSCS student
+            </p >
 
-            </p>
             <a href="mailto:xzhou411@gatech.edu" className="mylinks"> <AiOutlineMail /> Email</a>
             <br />
             <a href="https://linkedin.com/in/xiangxian-zhou-088246a6" className="mylinks"><AiFillLinkedin /> LinkedIn</a>
@@ -26,13 +27,17 @@ const Personinfo = () => {
             <br />
             <a href="https://scholar.google.com/citations?user=jEqFraUAAAAJ&hl=en" className="mylinks" > <SiGooglescholar /> Google Scholar</a>
             <br />
+
             <br />
+            {/* <p className="introduction">
+                <FiMapPin /> Hangzhou, China</p> */}
             <p className="introduction"> Favorite Quote:</p>
             <p className="introduction">"In the face of power, I would always be at a disadvantage, I knew, but I was a born contrarian, and there’s no other way for me to
                 live except by taking an oppositional stance.”</p>
             <p className="introduction">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-Ai Weiwei </p>
-        </Container>
+
+        </Container >
     )
 }
 
